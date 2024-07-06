a4e9233b12a0970b6f980638a950dbe7371d74f5
eric-armendariz <earmen3@illinois.edu>
2024-07-06 00:02:11 -0500

    Added login http function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,8 +1,31 @@
 import User from '../models/user.model.js'
 import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from '../utils/generateToken.js';
 
-export const login = (req, res) => {
-    console.log("loginUser");
+export const login = async (req, res) => {
+    try {
+        const {username, password} = req.body;
+        const user = await User.findOne({username});
+
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+        if (!user || !isPasswordCorrect) {
+            return res.status(400).json({error: "Invalid username or password"});
+        }
+
+        generateTokenAndSetCookie(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            username: user.username,
+            profilePic: user.profilePic,
+        });
+
+    } catch (error) {
+        console.log("Error in login controller", error.message)
+        res.status(500).json({error: "internal server error"})
+    }
 };
 
 export const logout = (req, res) => {
@@ -45,7 +68,9 @@ export const signup = async (req, res) => {
 
         //Save User and return some of its properties
         if (newUser) {
+            generateTokenAndSetCookie(newUser._id, res);
             await newUser.save();
+
             res.status(201).json({
                 _id: newUser._id,
                 fullName: newUser.fullName,
