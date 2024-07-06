e36fb313aa2329ade05cb2c36d40ad1e73abebb0
eric-armendariz <earmen3@illinois.edu>
2024-07-05 22:11:20 -0500

    Connected to mongoDB db

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,21 @@
-const express = require("express");
-const app = express();
+import express from 'express';
+import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from './db/connectToMongoDB.js';
+import dotenv from 'dotenv';
 
+dotenv.config();
+
+const app = express();
 const PORT = 5000;
 
 app.get("/", (req, res) => {
     res.send("Hello World!!!")
 });
 
-app.get("/api/auth/signup", (req, res) => {
-    console.log("signup route");
+app.use("/api/auth", authRoutes)
+
+app.listen(5000, () => {
+    connectToMongoDB();
+    console.log(`Server Running on port ${PORT}`)
 });
 
-app.get("/api/auth/login", (req, res) => {
-    console.log("login route");
-});
-
-app.get("/api/auth/logout", (req, res) => {
-    console.log("logout route");
-});
-
-app.listen(5000, () => console.log("Server Running on port 5000"));
