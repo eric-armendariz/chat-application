460fad05503fe886af94542824f97d138e9a72bc
eric-armendariz <earmen3@illinois.edu>
2024-07-11 12:57:28 -0500

    Implemented Get Conversations connection to backend

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Messages from './Messages.jsx';
 import MessageInput from './MessageInput.jsx';
 import { TiMessages } from "react-icons/ti";
+import useConversation from '../../zustand/useConversation.js';
 
 const MessageContainer = () => {
-    const noChatSelected = false;
+    const {selectedConversation, setSelectedConversation} = useConversation();
+
+    //Unmounts selected conversation on logout
+    useEffect(() => {
+        return () => setSelectedConversation(null)
+    }, [setSelectedConversation]);
+
   return (
     <div className='md:min-[450px] flex flex-col'>
-        {noChatSelected ? (
+        {!selectedConversation ? (
             <NoChatSelected />
         ) : (
             <>
             {/* Header */}
                 <div className='bg-slate-500 px-4 py-2 mb-2'>
                     <span className='label-text'>To:</span>{" "}
-                    <span className='text-gray-900 font-bold'>John Doe</span>
+                    <span className='text-gray-900 font-bold'>{selectedConversation.fullName}</span>
                 </div>
 
                 <Messages />
